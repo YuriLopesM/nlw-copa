42b7e3d8d62e0d2d5256ba94f178f0d47f3c43bc
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2022-11-04 12:12:51 -0300

    chore: eliminate git submodule for mobile

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,22 +1,22 @@
 interface HomeProps {
-    count: number
+  count: number
 }
 
 export default function Home(props: HomeProps) {
-    return (
-        <h1>Contagem: {props.count}</h1>
-    )
+  return (
+    <h1>Contagem: {props.count}</h1>
+  )
 }
 
 export const getServerSideProps = async () => {
-    const response = await fetch('http://localhost:3333/pools/count')
-    const data = await response.json()
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
 
-    console.log(data)
-    return {
-        props: {
-            count: data.count
-        }
+  console.log(data)
+  return {
+    props: {
+      count: data.count
     }
+  }
 }
-    
+
