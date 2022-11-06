7d9414740fabecf626291380bd5f0e3e7daa3979
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2022-11-06 15:41:20 -0300

    feat(server): finished routes and validations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,12 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { z } from 'zod'
-import { PrismaClient } from '@prisma/client'
-import ShortUniqueId from 'short-unique-id'
+import jwt from '@fastify/jwt'
 
-
-const prisma = new PrismaClient({
-  log: ['query'],
-})
+import { poolRoutes } from './routes/pool'
+import { authRoutes } from './routes/auth'
+import { userRoutes } from './routes/user'
+import { gameRoutes } from './routes/game'
+import { guessRoutes } from './routes/guess'
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -18,43 +17,16 @@ async function bootstrap() {
     origin: true
   })
 
-  fastify.get('/pools/count', async () => {
-    const count = await prisma.pool.count()
-
-    return { count }
+  // Em produção, o JWT_SECRET deve ser uma variável de ambiente
+  await fastify.register(jwt, {
+    secret: 'nlwcopa',
   })
 
-  fastify.get('/users/count', async () => {
-    const count = await prisma.user.count()
-
-    return { count }
-  })
-
-  fastify.get('/guesses/count', async () => {
-    const count = await prisma.guess.count()
-
-    return { count }
-  })
-
-  fastify.post('/pools', async (request, reply) => {
-    const createPoolBody = z.object({
-      title: z.string()      
-    })
-
-    const { title } = createPoolBody.parse(request.body)
-
-    const generate = new ShortUniqueId({ length: 6 })
-    const code = String(generate()).toUpperCase()
-
-    await prisma.pool.create({
-      data: {
-        title,
-        code: code
-      }
-    })
-
-    return reply.status(201).send({ code })
-  })
+  await fastify.register(authRoutes)
+  await fastify.register(gameRoutes)
+  await fastify.register(guessRoutes)
+  await fastify.register(poolRoutes)
+  await fastify.register(userRoutes)
 
   await fastify.listen({ port: 3333, /* host: '0.0.0.0' */ })
 }
