3c143bf4c1313f0330fcc80bc65437de75958ff6
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2022-11-05 21:31:28 -0300

    feat(mobile): added components, sign inand authentication, search and find layout

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,12 @@
 import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
+import { AuthContextProvider } from './src/contexts/AuthContext';
+
+import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme'
-import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -15,12 +17,14 @@ export default function App() {
 
     return (
         <NativeBaseProvider theme={THEME}>
+          <AuthContextProvider>
             <StatusBar
                 barStyle='light-content'
                 backgroundColor='transparent'
                 translucent
             />
-            {fontsLoaded ? <SignIn /> : <Loading />}
+            {fontsLoaded ? <Pools /> : <Loading />}
+          </AuthContextProvider>
         </NativeBaseProvider>
     );
 }
