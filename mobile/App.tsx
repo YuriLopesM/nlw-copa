592795475a50b984e04579f5fdfac93dad2cd598
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2022-11-06 16:46:27 -0300

    feat(mobile): added routing, finished authentication flow

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,7 +3,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
-import { Pools } from './src/screens/Pools';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme'
@@ -23,7 +23,7 @@ export default function App() {
                 backgroundColor='transparent'
                 translucent
             />
-            {fontsLoaded ? <Pools /> : <Loading />}
+            {fontsLoaded ? <Routes /> : <Loading />}
           </AuthContextProvider>
         </NativeBaseProvider>
     );
